4b7cac1553c3560706b00cae7b57758bb9946005
Jarthianur <Jarthianur@users.noreply.github.com>
2017-06-10 10:25:23 +0200

    bin dir

--- src/BME280-If/install.sh
+++ src/BME280-If/install.sh
@@ -2,4 +2,4 @@
 APR_CFG=$(apr-1-config --cflags --cppflags --includes --link-ld)
 #Where i2c-tools is installed from github e.g. /home/pi/i2c-tools/include/
 INCLUDE_PATH=
-gcc -std=c11 -o bme280-server -I $INCLUDE_PATH ../server/server.c ../i2c/i2cif.c bme280.c main.c $APR_CFG -li2c
+gcc -std=c11 -o bin/bme280-server -I $INCLUDE_PATH ../server/server.c ../i2c/i2cif.c bme280.c main.c $APR_CFG -li2c
