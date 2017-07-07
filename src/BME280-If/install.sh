89a38f1430ed875212cbaa2ecb38b1e2976e965e
Jarthianur <jarthianur.github@gmail.com>
2017-07-07 07:47:18 +0200

    add out file name

--- src/BME280-If/install.sh
+++ src/BME280-If/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 APR_CFG=$(apr-1-config --cflags --cppflags --includes --link-ld)
-gcc -std=c11 ../server/server.c ../i2c/i2cif.c bme280.c main.c $APR_CFG
+gcc -std=c11 -o bmeserve ../server/server.c ../i2c/i2cif.c bme280.c main.c $APR_CFG
