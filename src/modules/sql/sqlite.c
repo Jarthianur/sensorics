a4e765a250eeda3b68466ccc60dbad0505b0be4e
Jarthianur <jarthianur.github@gmail.com>
2018-12-30 17:41:40 +0100

    impl buffer

--- src/modules/sql/sqlite.c
+++ src/modules/sql/sqlite.c
@@ -22,6 +22,7 @@
 #include "sql/sqlite.h"
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 
 bool_t SQL_open(sql_db* db)
--- src/modules/util/buffer.c
+++ src/modules/util/buffer.c
@@ -21,16 +21,16 @@
 
 #include "util/buffer.h"
 
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+bool_t BUF_maybe_realloc(buffer* buf, size_t len);
+
 bool_t BUF_new(buffer* buf, size_t init)
 {
-    if (buf->data != NULL)
-    {
-        return FALSE;
-    }
-    if ((buf->data = malloc(sizeof(char) * init)) == NULL)
+    if ((buf->data = calloc(init, sizeof(char))) == NULL)
     {
         return FALSE;
     }
@@ -42,29 +42,20 @@ bool_t BUF_new(buffer* buf, size_t init)
 void BUF_free(buffer* buf)
 {
     free(buf->data);
+    buf->data      = NULL;
     buf->length    = 0;
     buf->allocated = 0;
 }
 
 bool_t BUF_copy(const buffer* src, buffer* dest)
 {
-    if (src->data == NULL)
+    if (!BUF_maybe_realloc(dest, src->length))
     {
-        dest->length = 0;
-        return TRUE;
-    }
-    if (dest->data == NULL || dest->allocated < src->length)
-    {
-        char* new_data = realloc(dest->data, src->length);
-        if (new_data == NULL)
-        {
-            return FALSE;
-        }
-        dest->data      = new_data;
-        dest->allocated = src->length;
+        return FALSE;
     }
     strncpy(dest->data, src->data, dest->allocated);
-    dest->length = src->length;
+    dest->length                                         = src->length;
+    dest->data[dest->length == 0 ? 0 : dest->length - 1] = 0;
     return TRUE;
 }
 
@@ -85,6 +76,10 @@ void BUF_move(buffer* src, buffer* dest)
 void BUF_clear(buffer* buf)
 {
     buf->length = 0;
+    if (buf->allocated > 0)
+    {
+        buf->data[0] = 0;
+    }
 }
 
 void BUF_shrink(buffer* buf)
@@ -96,10 +91,73 @@ void BUF_shrink(buffer* buf)
     }
 }
 
-bool_t BUF_append(const buffer* src, buffer* dest) {}
+bool_t BUF_append(const buffer* src, buffer* dest)
+{
+    size_t len = dest->length + src->length - 1;  // only 1 null byte
+    if (!BUF_maybe_realloc(dest, len))
+    {
+        return FALSE;
+    }
+    strncat(dest->data, src->data, dest->allocated);
+    return TRUE;
+}
 
-bool_t BUF_append_cstr(const char* src, buffer* dest) {}
+bool_t BUF_append_cstr(const char* src, buffer* dest)
+{
+    size_t len = dest->length + strlen(src);
+    if (!BUF_maybe_realloc(dest, len))
+    {
+        return FALSE;
+    }
+    strncat(dest->data, src, dest->allocated);
+    return TRUE;
+}
 
-bool_t BUF_remove(buffer* buf, size_t pos, size_t len) {}
+bool_t BUF_remove(buffer* buf, size_t pos, size_t len)
+{
+    if (pos >= buf->length - 1)
+    {
+        return FALSE;
+    }
+    if (len == 0 || pos + len >= buf->length)
+    {
+        buf->data[pos] = 0;
+        buf->length    = pos + 1;
+        return TRUE;
+    }
+    memmove(buf->data + pos, buf->data + pos + len, buf->length - pos - len);
+    buf->length -= len;
+    buf->data[buf->length - 1] = 0;  // just to be sure
+    return TRUE;
+}
 
-bool_t BUF_sprintf(buffer* buf, const char* fmt, ...) {}
+bool_t BUF_sprintf(buffer* buf, const char* fmt, ...)
+{
+    va_list args, args_cp;
+    va_start(args, fmt);
+    va_copy(args_cp, args);
+    s32_t len = vsnprintf(NULL, 0, fmt, args) + 1;
+    va_end(args);
+    if (len < 0 && !BUF_maybe_realloc(buf, len))
+    {
+        return FALSE;
+    }
+    vsnprintf(buf->data, buf->allocated, fmt, args_cp);
+    va_end(args_cp);
+    return TRUE;
+}
+
+bool_t BUF_maybe_realloc(buffer* buf, size_t len)
+{
+    if (buf->data == NULL || buf->allocated < len)
+    {
+        char* new_data = realloc(buf->data, len);
+        if (new_data == NULL)
+        {
+            return FALSE;
+        }
+        buf->data      = new_data;
+        buf->allocated = len;
+    }
+    return TRUE;
+}
